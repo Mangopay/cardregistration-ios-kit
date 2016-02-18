fdb2d5bd3ef8287762d49e0eb955c2278cea55d7
Victor <victorcampeanu@me.com>
2016-02-18 12:59:14 +0200

    Error handling

--- MangopayDemoApp/MangopayDemoApp/ViewController.m
+++ MangopayDemoApp/MangopayDemoApp/ViewController.m
@@ -13,7 +13,7 @@
 static NSString * const language = @"en";
 static NSString * const email = @"cata_craciun@hotmail.com";
 static NSString * const currency = @"EUR";
-static NSString * const URLString = @"https://sample.com/";
+static NSString * const URLString = @"https://sample";
 
 
 @interface ViewController ()
@@ -27,72 +27,89 @@ static NSString * const URLString = @"https://sample.com/";
  
     [self.activityIndicator startAnimating];
     
-    [self generateCardRegistration];
+    
+    [self generateCardRegistration:^(NSDictionary *responseObject, NSHTTPURLResponse *httpResp, NSError* error) {
+        
+        if (error) {
+            NSLog(@"URL Session Task Failed: %@", [error localizedDescription]);
+            dispatch_async( dispatch_get_main_queue(), ^{
+                [self.activityIndicator stopAnimating];
+            });
+            return;
+        }
+        
+        if (httpResp.statusCode != 200) {
+            NSLog(@"Handle status code %ld", httpResp.statusCode);
+            dispatch_async( dispatch_get_main_queue(), ^{
+                [self.activityIndicator stopAnimating];
+            });
+            return;
+        }
+        
+        // initiate MPAPIClient with received cardObject
+        self.mangopayClient = [[MPAPIClient alloc] initWithCardObject:responseObject];
+        
+        // collect card info from the user
+        [self.mangopayClient.cardObject setCardNumber:@"4970100000000154"];
+        [self.mangopayClient.cardObject setCardExpirationDate:@"1016"];
+        [self.mangopayClient.cardObject setCardCvx:@"123"];
+        
+        // register card
+        [self.mangopayClient registerCard:^(NSDictionary *response, NSError *error) {
+        
+            if (error) {
+                NSLog(@"Error: %@", error);
+            }
+            else { // card was VALIDATED
+                NSLog(@"VALIDATED %@", response);
+            }
+            
+            dispatch_async( dispatch_get_main_queue(), ^{
+                [self.activityIndicator stopAnimating];
+            });
+        }];
+    }];
 }
 
-- (void)generateCardRegistration
+- (void)generateCardRegistration:(void (^)(NSDictionary *responseDictionary, NSHTTPURLResponse *httpResp, NSError* error)) completionHandler
 {
     NSURLSessionConfiguration* sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
     sessionConfig.timeoutIntervalForRequest = 60.0;
     NSURLSession* session = [NSURLSession sessionWithConfiguration:sessionConfig delegate:nil delegateQueue:nil];
     
     NSURL* URL = [NSURL URLWithString:URLString];
-    NSDictionary* URLParams = @{MP_UrlParamLanguage: language,
-                                MP_UrlParamEmail: email,
-                                MP_UrlParamCurrency: currency,};
+    NSDictionary* URLParams = @{@"lang": language,
+                                @"customerEmail": email,
+                                @"currency": currency,};
     
-    URL = NSURLByAppendingQueryParameters(URL, URLParams);
+    URL = [MPAPIClient NSURLByAppendingQueryParameters:URL queryParameters:URLParams];
     NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:URL];
     request.HTTPMethod = @"GET";
     
-    /* Start a new Task */
     NSURLSessionDataTask* task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-        
+
         if (error == nil) { // Success
             NSHTTPURLResponse *httpResp = (NSHTTPURLResponse*) response;
             if (httpResp.statusCode == 200) {
                 
-                NSDictionary* responseObject = objectFromJSONdata(data);
-                if (responseObject) {
-                
-                    // initiate MPAPIClient with received cardObject
-                    self.mangopayClient = [[MPAPIClient alloc] initWithCardObject:responseObject];
-                    
-                    // collect card info from the user
-                    [self.mangopayClient.cardObject setCardNumber:@"4970100000000154"];
-                    [self.mangopayClient.cardObject setCardExpirationDate:@"1016"];
-                    [self.mangopayClient.cardObject setCardCvx:@"123"];
-
-                    // register card
-                    [self.mangopayClient registerCard:^(NSDictionary *response, MPErrorType error) {
-                        
-                        if (error) {
-                            NSLog(@"final error: %ld", error);
-                        }
-                        else {
-                            NSLog(@"final response %@", response);
-                            NSLog(@"final no error %ld", error);
-                        }
-                        
-                        dispatch_async( dispatch_get_main_queue(), ^{
-                            [self.activityIndicator stopAnimating];
-                        });
-                    }];
+                if (data) {
+                    NSDictionary* responseObject = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:NULL];
+                    completionHandler(responseObject, httpResp, nil);
+                }
+                else {
+                    NSError *error = [NSError errorWithDomain:@"MP"
+                                                         code:[(NSHTTPURLResponse*)response statusCode]
+                                                     userInfo:@{NSLocalizedDescriptionKey: @"Invalid response data"}];
+                    completionHandler(nil, httpResp, error);
                 }
             }
-            else {
-                NSLog(@"Handle status code %ld", httpResp.statusCode);
-            }
+            else
+                completionHandler(nil, httpResp, nil);
         }
-        else { // Failure
-            NSLog(@"URL Session Task Failed: %@", [error localizedDescription]);
-        }
+        else
+            completionHandler(nil, nil, error);
     }];
     [task resume];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
 }
 
 @end
