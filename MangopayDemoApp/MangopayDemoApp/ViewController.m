b27e545cf88b49b1cd3de20fa5c60a35dbf3da91
Victor <victorcampeanu@me.com>
2016-04-22 08:51:05 +0300

    added ATS settings in Info.plist, disabled all arbitrary loads

--- MangopayDemoApp/MangopayDemoApp/ViewController.m
+++ MangopayDemoApp/MangopayDemoApp/ViewController.m
@@ -78,7 +78,7 @@ static NSString * const serverURL = @"http://demo-mangopay.rhcloud.com/card-regi
     request.HTTPMethod = @"GET";
     
     NSURLSessionDataTask* task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-
+NSLog(@"response %@", response);
         if (error == nil) { // Success
             NSHTTPURLResponse *httpResp = (NSHTTPURLResponse*) response;
             if (httpResp.statusCode == 200) {
@@ -97,8 +97,9 @@ static NSString * const serverURL = @"http://demo-mangopay.rhcloud.com/card-regi
             else
                 completionHandler(nil, httpResp, nil);
         }
-        else
+        else { NSLog(@"error %@", error);
             completionHandler(nil, nil, error);
+        }
     }];
     [task resume];
 }
